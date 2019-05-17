ad181763db68d6b29d192fbcd22ceae1e7d1d15d
Wheezy <mateusz.wedel@gmail.com>
2019-05-17 22:12:03 +0200

    Use custom db while testing.

--- src/test/java/pl/wedel/CustomerRepositoryIntegrationTest.java
+++ src/test/java/pl/wedel/CustomerRepositoryIntegrationTest.java
@@ -4,13 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.wedel.data.CustomerRepository;
 import pl.wedel.model.Customer;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CustomerRepositoryIntegrationTest
 {
     @Autowired
