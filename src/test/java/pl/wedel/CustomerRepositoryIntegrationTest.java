734283ade587dd6cf68b7a89cd4baaf8e22b66ef
Wheezy <mateusz.wedel@gmail.com>
2019-05-25 14:07:03 +0200

    Check badge change.

--- src/test/java/pl/wedel/CustomerRepositoryIntegrationTest.java
+++ src/test/java/pl/wedel/CustomerRepositoryIntegrationTest.java
@@ -27,6 +27,6 @@ public class CustomerRepositoryIntegrationTest
         
         Customer customer = customerRepository.findOne(1L);
     
-        Assert.assertNotNull(customer);
+        Assert.assertNull(customer);
     }
 }
