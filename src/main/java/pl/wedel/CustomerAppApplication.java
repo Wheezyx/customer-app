5c24519aba0d00a2adb3c7bd3f3d78e06edf023c
Wheezy <mateusz.wedel@gmail.com>
2019-05-27 19:19:43 +0200

    Add rabbitmq support.

--- src/main/java/pl/wedel/CustomerAppApplication.java
+++ src/main/java/pl/wedel/CustomerAppApplication.java
@@ -1,7 +1,9 @@
 package pl.wedel;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerAppApplication {
@@ -9,4 +11,22 @@ public class CustomerAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerAppApplication.class, args);
 	}
+	
+	@Bean
+	public CachingConnectionFactory cachingConnectionFactory()
+	{
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+		
+		String uri = System.getenv("CLOUDAMQP_URL");
+		if(uri == null)
+		{
+			uri = "amqp://guest:guest@localhost";
+		}
+		
+		cachingConnectionFactory.setUri(uri);
+		return cachingConnectionFactory;
+	}
+	
 }
+
+
--- src/main/java/pl/wedel/controller/HomeController.java
+++ src/main/java/pl/wedel/controller/HomeController.java
@@ -1,15 +1,20 @@
 package pl.wedel.controller;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+    
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    
     @GetMapping("/")
     public String home()
     {
+        rabbitTemplate.convertAndSend("TEST");
         return "index";
     }
 }
